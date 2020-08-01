687185ac01df1101c904c2c5d72f45cecaf3f97d
Dominic Dagradi <ddagradi@gmail.com>
2020-07-31 20:05:09 -0700

    Basic controls and data mapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,248 @@
-import React from 'react'
-import './App.scss'
+import React, { useMemo, useState } from 'react'
+
+import './styles/App.scss'
+import {
+  Prevalence,
+  RiskProfile,
+  Interaction,
+  Setting,
+  Distance,
+  TheirMask,
+  YourMask,
+} from './data'
+
+const Card: React.FunctionComponent<{ title: string }> = (props) => (
+  <div className='card mb-3'>
+    <div className='card-header'>{props.title}</div>
+    <div className='card-body'>{props.children}</div>
+  </div>
+)
 
 export const App = () => {
-  return <div className='App'></div>
+  // Location risk state
+  const [prevalence, setPrevalence] = useState<string>('sf')
+
+  // Person risk state
+  const [riskProfile, setRiskProfile] = useState<string>('average')
+  const [interaction, setInteraction] = useState<string>('oneTime')
+  const [personCount, setPersonCount] = useState<number>(1)
+
+  // Activity risk state
+  const [setting, setSetting] = useState<string>('outdoor')
+  const [distance, setDistance] = useState<string>('normal')
+  const [duration, setDuration] = useState<number>(60)
+  const [theirMask, setTheirMask] = useState<string>('masked')
+  const [yourMask, setYourMask] = useState<string>('masked')
+
+  const points = useMemo(() => {
+    let points = Prevalence[prevalence].multiplier
+
+    // Person risk
+    points *= personCount
+    points *= RiskProfile[riskProfile].multiplier
+
+    // Activity risk
+    points *= Setting[setting].multiplier
+    points *= Distance[distance].multiplier
+    points *= TheirMask[theirMask].multiplier
+    points *= YourMask[yourMask].multiplier
+
+    // Duration + interaction type
+    if (interaction === 'repeated') {
+      points *= Interaction[interaction].multiplier
+    } else {
+      points *=
+        Interaction[interaction].multiplier *
+        Math.min((duration || 60) / 60.0, 4)
+    }
+
+    return points > 10 ? Math.round(points) : points.toFixed(2)
+  }, [
+    prevalence,
+    riskProfile,
+    interaction,
+    personCount,
+    setting,
+    distance,
+    duration,
+    theirMask,
+    yourMask,
+  ])
+
+  return (
+    <div className='App'>
+      <div className='container'>
+        <div className='row justify-content-center'>
+          <div className='col-md-6'>
+            <h1 className='mt-4'>microCOVID Calculator</h1>
+
+            <p>
+              It can be pretty annoying to calculate microCOVIDs for every
+              activity you’re considering. With that in mind, we developed a
+              calculator tool to help you estimate and multiply the person risk,
+              activity risk, and any discounts, to get an estimated number of
+              microCOVIDs from a given activity.
+            </p>
+
+            <Card title='Location/Prevalence'>
+              <p>
+                Prevalence options are rough estimates for a given place and
+                time.
+              </p>
+              <div className='form-group'>
+                <select
+                  id='prevalence'
+                  className='form-control form-control-lg'
+                  onChange={(e) => setPrevalence(e.target.value)}
+                  value={prevalence}
+                >
+                  {Object.keys(Prevalence).map((value, index) => (
+                    <option key={index} value={value}>
+                      {Prevalence[value].label}
+                    </option>
+                  ))}
+                  <optgroup label=''></optgroup>
+                </select>
+              </div>
+            </Card>
+
+            <Card title='Person Risk'>
+              <div className='form-group'>
+                <label htmlFor='personCount'>Number of people</label>
+                <input
+                  className='form-control form-control-lg'
+                  type='number'
+                  value={personCount}
+                  onChange={(e) => setPersonCount(parseInt(e.target.value))}
+                />
+              </div>
+
+              <div className='form-group'>
+                <label htmlFor='riskProfile'>Person(s) Risk Profile</label>
+                <select
+                  id='riskProfile'
+                  className='form-control form-control-lg'
+                  onChange={(e) => setRiskProfile(e.target.value)}
+                  value={riskProfile}
+                >
+                  {Object.keys(RiskProfile).map((value, index) => (
+                    <option key={index} value={value}>
+                      {RiskProfile[value].label}
+                    </option>
+                  ))}
+                  <optgroup label=''></optgroup>
+                </select>
+              </div>
+
+              <div className='form-group'>
+                <label htmlFor='interactionType'>
+                  Frequency of Interaction
+                </label>
+                <select
+                  id='interactionType'
+                  className='form-control form-control-lg'
+                  onChange={(e) => setInteraction(e.target.value)}
+                  value={interaction}
+                >
+                  {Object.keys(Interaction).map((value, index) => (
+                    <option key={index} value={value}>
+                      {Interaction[value].label}
+                    </option>
+                  ))}
+                  <optgroup label=''></optgroup>
+                </select>
+              </div>
+            </Card>
+
+            <Card title='Activity Risk'>
+              <div className='form-group'>
+                <label htmlFor='setting'>Settings</label>
+                <select
+                  id='setting'
+                  className='form-control form-control-lg'
+                  onChange={(e) => setSetting(e.target.value)}
+                  value={setting}
+                >
+                  {Object.keys(Setting).map((value, index) => (
+                    <option key={index} value={value}>
+                      {Setting[value].label}
+                    </option>
+                  ))}
+                  <optgroup label=''></optgroup>
+                </select>
+              </div>
+
+              <div className='form-group'>
+                <label htmlFor='distance'>Distance</label>
+                <select
+                  id='distance'
+                  className='form-control form-control-lg'
+                  onChange={(e) => setDistance(e.target.value)}
+                  value={distance}
+                >
+                  {Object.keys(Distance).map((value, index) => (
+                    <option key={index} value={value}>
+                      {Distance[value].label}
+                    </option>
+                  ))}
+                  <optgroup label=''></optgroup>
+                </select>
+              </div>
+
+              <div className='form-group'>
+                <label htmlFor='duration'>Duration (in minutes)</label>
+                <input
+                  className='form-control form-control-lg'
+                  type='number'
+                  value={duration}
+                  onChange={(e) => setDuration(parseInt(e.target.value))}
+                />
+              </div>
+
+              <div className='form-group'>
+                <label htmlFor='theirMask'>Their Mask</label>
+                <select
+                  id='theirMask'
+                  className='form-control form-control-lg'
+                  onChange={(e) => setTheirMask(e.target.value)}
+                  value={theirMask}
+                >
+                  {Object.keys(TheirMask).map((value, index) => (
+                    <option key={index} value={value}>
+                      {TheirMask[value].label}
+                    </option>
+                  ))}
+                  <optgroup label=''></optgroup>
+                </select>
+              </div>
+
+              <div className='form-group'>
+                <label htmlFor='yourMask'>Your Mask</label>
+                <select
+                  id='yourMask'
+                  className='form-control form-control-lg'
+                  onChange={(e) => setYourMask(e.target.value)}
+                  value={yourMask}
+                >
+                  {Object.keys(YourMask).map((value, index) => (
+                    <option key={index} value={value}>
+                      {YourMask[value].label}
+                    </option>
+                  ))}
+                  <optgroup label=''></optgroup>
+                </select>
+              </div>
+            </Card>
+
+            <Card title='Result'>
+              <h1>
+                {points} points
+                {interaction === 'repeated' && '/week'}
+              </h1>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
---
+++ src/data.ts
@@ -0,0 +1,57 @@
+interface FormValue {
+  label: string
+  multiplier: number
+}
+
+export const Prevalence: { [key: string]: FormValue } = {
+  sf: { label: 'San Francisco (July 2020)', multiplier: 614 },
+  sydney: { label: 'Sydney (July 2020)', multiplier: 13 },
+}
+
+export const RiskProfile: { [key: string]: FormValue } = {
+  average: { label: 'Any average person in your area', multiplier: 1 },
+  livingAlone: {
+    label: 'A person who lives alone, is isolating',
+    multiplier: 0.1,
+  },
+  livingInClosedHouse: {
+    label: 'A person who lives in a closed house (<6 people)',
+    multiplier: 0.1 + 0.03 * 5,
+  },
+  highRisk: { label: 'A high risk person', multiplier: 2 }, // FIXME
+}
+
+export const Interaction: { [key: string]: FormValue } = {
+  oneTime: { label: 'One-time contact', multiplier: 0.08 },
+  repeated: {
+    label: 'Repeated Contact (coworker, housemate, child, etc.)',
+    multiplier: 0.3,
+  },
+}
+
+export const Setting: { [key: string]: FormValue } = {
+  indoor: { label: 'Indoor', multiplier: 1 },
+  outdoor: { label: 'Outdoor', multiplier: 0.1 },
+}
+export const Distance: { [key: string]: FormValue } = {
+  normal: {
+    label: 'Normal socializing (~3 feet apart) most of the time',
+    multiplier: 1,
+  },
+  sixFt: { label: '6+ feet apart most of the time', multiplier: 0.5 },
+  tenFt: { label: '10+ feet apart most of the time', multiplier: 0.25 },
+}
+export const TheirMask: { [key: string]: FormValue } = {
+  none: { label: 'None (including bandanas & buffs)', multiplier: 1 },
+  masked: {
+    label: 'Masked (surgical, double layer cotton, etc)',
+    multiplier: 0.2,
+  },
+}
+export const YourMask: { [key: string]: FormValue } = {
+  none: { label: 'None (including bandanas & buffs)', multiplier: 1 },
+  masked: {
+    label: 'Masked (surgical, double layer cotton, etc)',
+    multiplier: 0.5,
+  },
+}
