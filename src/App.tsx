451922dafdf15a06fe7416ed523c782ce5bbd821
Dominic Dagradi <ddagradi@gmail.com>
2020-07-31 17:31:58 -0700

    Sass, Bootstrap, Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+  return <div className='App'></div>
 }
-
-export default App;
