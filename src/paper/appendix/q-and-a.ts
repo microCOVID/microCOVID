a5f74aedc73c02a87c83c402e7123a486d2d0303
RhysLindmark <rhyslindmark@gmail.com>
2020-08-22 16:25:47 -0700

    first push Q&A

---
+++ src/paper/appendix/q-and-a.ts
@@ -0,0 +1,28 @@
+const title = 'Q&A'
+
+const content = `
+
+### General
+**If there’s a precaution I don’t see a multiplier for here, should I still do it?**
+
+Yes, definitely! The most important such precaution, in our opinion, is communication around symptoms:
+- Ask people directly and clearly if they have any symptoms, right before hanging out with them. Even if they only have “mild” symptoms that seem like “nothing to worry about”, consider if you can take a rain check, or at least MODify your hangout.
+- If you have any more concerning symptoms then immediately notify anyone you saw in the past 10 days, and contact your doctor for medical advice.
+- If a household member develops more concerning symptoms, follow CDC best practices for home isolation straight away while you contact a doctor for medical advice.
+
+Other important precautions include washing & sanitizing your hands, covering your sneezes with your elbow including when you are at home, using a better-fitting mask, and wearing eye protection (we particularly recommend wearing goggles and a P100 mask if you are looking for extra safety when shopping!)
+
+
+[^1]:  More concerning symptoms include cough, chest tightness/discomfort, obvious sore throat, body aches, malaise, loss of taste/smell, nausea/vomiting, loss of appetite, diarrhea, any subjective “feverish feeling” or elevated temperature, fainting, or thermometer reading of >100.4.
+[^2]:
+
+
+
+
+`
+
+
+const post = { title, content}
+
+
+export default post
