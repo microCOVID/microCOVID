272f6c4d9f6ac18c76ad1d7752251e8ea5dde7c4
Anna Thundersnow <anna.tsykalova@gmail.com>
2020-08-22 22:36:50 -0700

    small proofreading edits for intro

--- src/paper/1-intro.ts
+++ src/paper/1-intro.ts
@@ -12,33 +12,34 @@ const content = `
 
 ---
 
-You’re already familiar with some rules of thumb for avoiding COVID-19 infection: wear a mask, stay 6 feet apart, and only socialize outdoors. But *just how much* do these things help? How big a deal is it if you remove your mask when hanging out with a friend? What happens if you get closer than 6ft? Bars and restaurants are opening, but is it safe to go inside? It’s tough to make good choices when you don’t know how large or small the risks really are.
+You’re already familiar with some rules of thumb for avoiding COVID-19 infection: wear a mask, stay 6 feet apart, and only socialize outdoors. But *just how much* do these things help? How big a deal is it if you remove your mask when hanging out with a friend? What happens if you get closer than 6 feet? Bars and restaurants are opening, but is it safe to go inside? It’s tough to make good choices when you don’t know how large or small the risks really are.
 
 We—the authors—were really struggling with this. We wanted a rigorous way to make decisions about COVID risk. So we did some research and crunched some numbers.  We trawled the scientific literature for data.  We spent hours estimating the COVID risk of various activities. And in this blog post, we’ll share our research (and some practical tools!) with you.
 
 We’ll show you:
 
-- How we measure COVID infection risk, in units of “microCOVIDs”
-- How risky we think various common activities are
-- How you can estimate the COVID risk of your own actions (using our handy-dandy [COVID risk calculator](/calculator))
-- And, most importantly, how to make decisions that balance freedom and fun with safety and health.
+- how we measure COVID infection risk, in units of “microCOVIDs”
+- how risky we think various common activities are
+- how you can estimate the COVID risk of your own actions (using our handy-dandy [COVID risk calculator](/calculator))
+- and, most importantly, how to make decisions that balance freedom and fun with safety and health.
 
 You’re in the target audience of this post if you are comfortable with numbers and want to think about how your personal choices affect your chance of getting COVID-19. We are not focusing on society-wide pandemic dynamics, policy responses, or suggestions for public health officials.
 
 This is the first part of a two-part post. In the second part [TODO link], we’ll go into more detail about how we use these COVID risk estimates as part of a “risk budget” system for our communal house.
 
 If you want to skip the intro & framing, and jump to our final estimates of COVID risk, click here. [TODO create link.]
-If you want to skip the entire article and just use the calculator tool, click here. [TODO create link.]
+
+If you want to skip the entire article and just use the calculator tool, click [here](/calculator).
 
 
 ## Disclaimer
-_This article (and corresponding calculator) was collaboratively written by the members and friends of Ibasho, a communal house in San Francisco. Our goal is to protect our community’s physical and mental health during the pandemic, by promoting balanced evidence-based decision-making by individual citizens. We have based our numbers in this article on scientific research; however, none of us are epidemiologists and our model is still a guess and not an authoritative “truth”. Please continue to follow government guidance._
+_This article (and corresponding calculator) was collaboratively written by the members and friends of Ibasho, a communal house in San Francisco. Our goal is to protect our broader community’s physical and mental health during the pandemic, by promoting balanced evidence-based decision-making by individual members. We have based our numbers in this article on scientific research; however, none of us are epidemiologists and our model is still a guess and not an authoritative “truth”. Please continue to follow government guidance._
 
 ## How to navigate this whitepaper
 
 Use the "previous" and "next" links at the bottom of the page to navigate from one section to the next.
 
-Footnotes[^demo] usually contain nerdy details for detail-loving readers, and often a link to *even more gory detail* in the TODO Research Sources section.
+Footnotes[^demo] usually contain nerdy details for detail-loving readers, and often a link to *even more gory detail* in the TResearch Sources section [TODO link].
 
 
 [^demo]: This is an example footnote! Click the arrow to return to where you came from in the text:
