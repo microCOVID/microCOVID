e24579bf9f8b6392f2e3a41580836fe572051b29
Dominic Dagradi <ddagradi@gmail.com>
2020-08-23 15:00:50 -0700

    basic test case fixup

--- src/data/__tests__/calculate.test.ts
+++ src/data/__tests__/calculate.test.ts
@@ -1,10 +1,16 @@
-import { eq } from 'lodash'
-
 import { CalculatorData, calculate } from 'data/calculate'
+import { ExampleLocations } from 'data/location'
+import { prepopulated } from 'data/prepopulated'
 
 describe('calculate', () => {
   it('returns the correct value', () => {
-    const data: CalculatorData = {}
+    const location = 'sf'
+    const scenario = 'Outdoor masked hangout with 2 people'
+    const data: CalculatorData = {
+      location,
+      ...ExampleLocations[location],
+      ...prepopulated[scenario],
+    }
 
     const response = calculate(data)
     expect(response).toBe(1)
