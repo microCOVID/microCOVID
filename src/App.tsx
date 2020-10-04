74f22c37d6746866600bc74ad5ba9f5697d3054b
Jeremy Blanchard <blanchard.jeremy@gmail.com>
2020-10-03 23:28:07 -0700

    Fix lint errors

--- src/App.tsx
+++ src/App.tsx
@@ -71,7 +71,7 @@ export const App = (): React.ReactElement => {
                         href={`/paper/${pageId}`}
                         key={pageIndex}
                       >
-                        {pageIndex+1}. {pages[pageId].shortTitle}
+                        {pageIndex + 1}. {pages[pageId].shortTitle}
                       </NavDropdown.Item>
                     ))}
                   </NavDropdown>
