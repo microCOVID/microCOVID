d1089773cd4242023fa4e5cf43e2fd7f5848f441
Benjamin Shaya <bshaya@alum.mit.edu>
2020-10-05 22:54:58 -0700

    Update calculator to make tests pass.

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,60 +1,60 @@
 export interface FormValue {
-  label: string
-  multiplier: number
+  label: string;
+  multiplier: number;
 }
 
 const formValue = function (label: string, multiplier: number): FormValue {
-  return { label, multiplier }
-}
+  return { label, multiplier };
+};
 
-const housemateMult = 0.3
+const housemateMult = 0.3;
 export const Interaction: { [key: string]: FormValue } = {
-  oneTime: { label: 'One-time interaction', multiplier: 0.06 },
+  oneTime: { label: "One-time interaction", multiplier: 0.06 },
   repeated: {
-    label: 'Household member',
+    label: "Household member",
     multiplier: housemateMult,
   },
   partner: {
-    label: 'Partner / spouse',
+    label: "Partner / spouse",
     multiplier: 0.48,
   },
-}
+};
 
 export const Setting: { [key: string]: FormValue } = {
-  indoor: { label: 'Indoor', multiplier: 1 },
-  outdoor: { label: 'Outdoor', multiplier: 0.05 },
-}
+  indoor: { label: "Indoor", multiplier: 1 },
+  outdoor: { label: "Outdoor", multiplier: 0.05 },
+};
 export const Distance: { [key: string]: FormValue } = {
-  intimate: formValue('Cuddling / intimate', 2),
-  normal: formValue('Normal socializing (~3ft/ ~1m apart)', 1),
-  sixFt: formValue('>6ft / 2m apart', 0.5),
-  tenFt: formValue('>10ft / 3m apart', 0.25),
-}
+  intimate: formValue("Intimate", 2),
+  normal: formValue("Normal socializing (~3ft/ ~1m apart)", 1),
+  sixFt: formValue(">6ft / 2m apart", 0.5),
+  tenFt: formValue(">10ft / 3m apart", 0.25),
+};
 
-const noneLabel = 'No mask or poorly-worn mask'
-const basicLabel = 'Cotton mask, bandana, or buff'
-const filteredLabel = 'Surgical mask or mask with PM2.5 filter insert'
+const noneLabel = "No mask or poorly-worn mask";
+const basicLabel = "Cotton mask, bandana, or buff";
+const filteredLabel = "Surgical mask or mask with PM2.5 filter insert";
 export const TheirMask: { [key: string]: FormValue } = {
   none: formValue(noneLabel, 1.0),
   basic: formValue(basicLabel, 0.25),
   filtered: formValue(filteredLabel, 0.25),
-}
+};
 export const YourMask: { [key: string]: FormValue } = {
   none: formValue(noneLabel, 1.0),
   basic: formValue(basicLabel, 1.0),
   filtered: formValue(filteredLabel, 0.5),
-}
+};
 export const Voice: { [key: string]: FormValue } = {
   silent: {
-    label: 'Not talking (such as quietly riding the train)',
+    label: "Not talking (such as quietly riding the train)",
     multiplier: 0.2,
   },
-  normal: { label: 'Normal conversation', multiplier: 1 },
+  normal: { label: "Normal conversation", multiplier: 1 },
   loud: {
-    label: 'Loud talking (shouting, talking over music, singing)',
+    label: "Loud talking (shouting, talking over music, singing)",
     multiplier: 5,
   },
-}
+};
 
 /*
  * Exposed to ten (silent distanced masked) average people indoors,
@@ -67,75 +67,75 @@ const livingAloneMult =
   TheirMask.basic.multiplier *
   YourMask.filtered.multiplier *
   Interaction.oneTime.multiplier *
-  Setting.indoor.multiplier
+  Setting.indoor.multiplier;
 
 export const RiskProfile: { [key: string]: FormValue } = {
   average: {
-    label: 'An average person in your area',
+    label: "An average person in your area",
     multiplier: 1,
   },
 
   frontline: {
-    label: 'An essential or front-line worker in your area',
+    label: "An essential or front-line worker in your area",
     multiplier: 3,
   },
 
   nonFrontline: {
-    label: 'Someone in your area who is NOT an essential or front-line worker',
+    label: "Someone in your area who is NOT an essential or front-line worker",
     multiplier: 0.5,
   },
 
   livingAlone: {
-    label: 'Lives alone and only grocery shops',
+    label: "Lives alone and only grocery shops",
     multiplier: livingAloneMult,
   },
 
   livingWithPartner: {
-    label: 'Lives with partner, both people only grocery shop',
+    label: "Lives with partner, both people only grocery shop",
     multiplier: (1 + 0.48) * livingAloneMult,
   },
 
   closedPod4: {
-    label: 'In a closed pod of 4 people (who do not go to work or socialize)',
+    label: "In a closed pod of 4 people (who do not go to work or socialize)",
     multiplier: (1 + 4 * housemateMult) * livingAloneMult,
   },
 
   closedPod10: {
-    label: 'In a closed pod of 10 people (who do not go to work or socialize)',
+    label: "In a closed pod of 10 people (who do not go to work or socialize)",
     multiplier: (1 + 10 * housemateMult) * livingAloneMult,
   },
 
   closedPod20: {
-    label: 'In a closed pod of 20 people (who do not go to work or socialize)',
+    label: "In a closed pod of 20 people (who do not go to work or socialize)",
     multiplier: (1 + 20 * housemateMult) * livingAloneMult,
   },
 
   contact1: {
     label:
-      'Has 1 close contact (incl. roommates) who socializes but does not work. No other work or socializing.',
+      "Has 1 close contact (incl. roommates) who socializes but does not work. No other work or socializing.",
     multiplier: housemateMult * 0.5 + livingAloneMult, // Housemate + grocery exposure
   },
 
   contact4: {
     label:
-      'Has 4 close contacts (incl. roommates) who socialize but do not work. No other work or socializing.',
+      "Has 4 close contacts (incl. roommates) who socialize but do not work. No other work or socializing.",
     multiplier: 4 * housemateMult * 0.5 + livingAloneMult, // Housemate + grocery exposure
   },
 
   contact10: {
     label:
-      'Has 10 close contacts (incl. roommates) who socialize but do not work. No other work or socializing.',
+      "Has 10 close contacts (incl. roommates) who socialize but do not work. No other work or socializing.",
     multiplier: 10 * housemateMult * 0.5 + livingAloneMult, // Housemate + grocery exposure
   },
 
   contactWorks: {
     label:
-      'Has 1 close contact (incl. roommates) who is an essential worker. No other work or socializing.',
+      "Has 1 close contact (incl. roommates) who is an essential worker. No other work or socializing.",
     multiplier: housemateMult * 3 + livingAloneMult, // Housemate + grocery exposure
   },
 
   bars: {
-    label: 'Goes to bars regularly',
+    label: "Goes to bars regularly",
     /*
      * Six hours of indoor exposure to a dozen people (2 near you, 10 six feet away)
      * who are not wearing masks and are talking loudly.
@@ -146,7 +146,7 @@ export const RiskProfile: { [key: string]: FormValue } = {
       (2 + 10 * Distance.sixFt.multiplier * Voice.loud.multiplier),
   },
   hasCovid: {
-    label: 'Has COVID',
+    label: "Has COVID",
     multiplier: -1,
   },
-}
+};
