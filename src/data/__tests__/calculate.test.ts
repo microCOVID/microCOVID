b638649b12d1d641beba4c72359eefe779607975
Benjamin Shaya <bshaya@alum.mit.edu>
2020-10-05 23:01:54 -0700

    Update husky to use the same lint rules as CI.
    Otherwise it guarantees that you'll fail CI.

--- src/data/__tests__/calculate.test.ts
+++ src/data/__tests__/calculate.test.ts
@@ -3,120 +3,120 @@ import {
   calculate,
   calculateLocationPersonAverage,
   defaultValues,
-} from "data/calculate";
-import { RiskProfile } from "data/data";
-import { prepopulated } from "data/prepopulated";
+} from 'data/calculate'
+import { RiskProfile } from 'data/data'
+import { prepopulated } from 'data/prepopulated'
 
-describe("calculate", () => {
+describe('calculate', () => {
   // Prevailance is .1% with 6x underreporting factor
   const exampleLocation = {
-    subLocation: "mock city",
-    topLocation: "mock state",
-    label: "mock city",
-    population: "1,000,000",
+    subLocation: 'mock city',
+    topLocation: 'mock state',
+    label: 'mock city',
+    population: '1,000,000',
     casesPastWeek: 999, // will add 1 in pseudocount
     casesIncreasingPercentage: 0,
     positiveCasePercentage: 1,
-  };
-  const expectedPrevalance = 0.006;
+  }
+  const expectedPrevalance = 0.006
 
-  it("compensates for underreporting", () => {
+  it('compensates for underreporting', () => {
     const data: CalculatorData = {
       ...defaultValues,
       ...exampleLocation,
-    };
+    }
     expect(calculateLocationPersonAverage(data)).toBeCloseTo(
-      expectedPrevalance * 1e6
-    );
-  });
+      expectedPrevalance * 1e6,
+    )
+  })
 
-  it("produces same results as by hand", () => {
-    const scenario = "Outdoor masked hangout with 2 people";
+  it('produces same results as by hand', () => {
+    const scenario = 'Outdoor masked hangout with 2 people'
     const data: CalculatorData = {
       ...exampleLocation,
       ...prepopulated[scenario],
-    };
+    }
 
-    const response = calculate(data);
+    const response = calculate(data)
     // average * 2 people * outdoor * 1 hr * their mask * your mask
-    expect(response).toBe(((((0.006 * 2) / 20) * 0.06) / 4 / 1) * 1e6);
-  });
+    expect(response).toBe(((((0.006 * 2) / 20) * 0.06) / 4 / 1) * 1e6)
+  })
 
   it.each`
     scenario                                                        | result
-    ${"Outdoor masked hangout with 2 people"}                       | ${9}
-    ${"Indoor unmasked hangout with 2 people"}                      | ${720}
-    ${"Car ride with 1 person for 15 mins"}                         | ${90}
-    ${"One-night stand with a random person"}                       | ${2880}
-    ${"Live-in partner who has no indoor interactions besides you"} | ${21.6}
-    ${"Grocery store for 60 minutes"}                               | ${45}
-    ${"Plane ride"}                                                 | ${1080}
-    ${"Eating in restaurant, outdoors"}                             | ${202.5}
-    ${"Eating in restaurant, indoors"}                              | ${4050}
-    ${"Going to bar"}                                               | ${27000}
-    ${"Large outdoor party: masked with 250 people"}                | ${3375}
-    ${"Small indoor party: unmasked with 25 people"}                | ${27000}
-    ${"Outdoor, masked hangout with person who has COVID"}          | ${750}
-    ${"Voting in-person"}                                           | ${1.5}
-    ${"Indoor, unmasked hangout with person who has COVID"}         | ${60000}
-  `("should return $result for $scenario", ({ scenario, result }) => {
+    ${'Outdoor masked hangout with 2 people'}                       | ${9}
+    ${'Indoor unmasked hangout with 2 people'}                      | ${720}
+    ${'Car ride with 1 person for 15 mins'}                         | ${90}
+    ${'One-night stand with a random person'}                       | ${2880}
+    ${'Live-in partner who has no indoor interactions besides you'} | ${21.6}
+    ${'Grocery store for 60 minutes'}                               | ${45}
+    ${'Plane ride'}                                                 | ${1080}
+    ${'Eating in restaurant, outdoors'}                             | ${202.5}
+    ${'Eating in restaurant, indoors'}                              | ${4050}
+    ${'Going to bar'}                                               | ${27000}
+    ${'Large outdoor party: masked with 250 people'}                | ${3375}
+    ${'Small indoor party: unmasked with 25 people'}                | ${27000}
+    ${'Outdoor, masked hangout with person who has COVID'}          | ${750}
+    ${'Voting in-person'}                                           | ${1.5}
+    ${'Indoor, unmasked hangout with person who has COVID'}         | ${60000}
+  `('should return $result for $scenario', ({ scenario, result }) => {
     const data: CalculatorData = {
       ...exampleLocation,
       ...prepopulated[scenario],
-    };
+    }
 
-    expect(calculate(data)).toBeCloseTo(result);
-  });
+    expect(calculate(data)).toBeCloseTo(result)
+  })
 
-  it("should produce a self-consistent living alone risk profile", () => {
+  it('should produce a self-consistent living alone risk profile', () => {
     const data: CalculatorData = {
       ...exampleLocation,
-      riskProfile: "average",
-      interaction: "oneTime",
+      riskProfile: 'average',
+      interaction: 'oneTime',
       personCount: 10,
 
-      setting: "indoor",
-      distance: "sixFt",
+      setting: 'indoor',
+      distance: 'sixFt',
       duration: 60,
-      theirMask: "basic",
-      yourMask: "filtered",
-      voice: "silent",
-    };
+      theirMask: 'basic',
+      yourMask: 'filtered',
+      voice: 'silent',
+    }
 
     expect(calculate(data)).toBeCloseTo(
-      expectedPrevalance * RiskProfile["livingAlone"]["multiplier"] * 1e6
-    );
-  });
+      expectedPrevalance * RiskProfile['livingAlone']['multiplier'] * 1e6,
+    )
+  })
 
-  it("should produce the same results for intimate outdoors vs indoors", () => {
+  it('should produce the same results for intimate outdoors vs indoors', () => {
     const indoorIntimate: CalculatorData = {
       ...exampleLocation,
-      ...prepopulated["One-night stand with a random person"],
-    };
+      ...prepopulated['One-night stand with a random person'],
+    }
     const outdoorIntimate: CalculatorData = {
       ...indoorIntimate,
-      setting: "outdoor",
-    };
+      setting: 'outdoor',
+    }
 
-    expect(calculate(outdoorIntimate)).toEqual(calculate(indoorIntimate));
-  });
+    expect(calculate(outdoorIntimate)).toEqual(calculate(indoorIntimate))
+  })
 
-  it("should treat intimate interactions as at least an hour", () => {
+  it('should treat intimate interactions as at least an hour', () => {
     const oneHourIntimate: CalculatorData = {
       ...exampleLocation,
-      ...prepopulated["One-night stand with a random person"],
+      ...prepopulated['One-night stand with a random person'],
       duration: 60,
-    };
+    }
     const oneMinuteIntimate: CalculatorData = {
       ...oneHourIntimate,
       duration: 1,
-    };
+    }
     const twoHourIntimate: CalculatorData = {
       ...oneHourIntimate,
       duration: 120,
-    };
+    }
 
-    expect(calculate(oneMinuteIntimate)).toEqual(calculate(oneHourIntimate));
-    expect(calculate(twoHourIntimate)).toEqual(calculate(oneHourIntimate)! * 2);
-  });
-});
+    expect(calculate(oneMinuteIntimate)).toEqual(calculate(oneHourIntimate))
+    expect(calculate(twoHourIntimate)).toEqual(calculate(oneHourIntimate)! * 2)
+  })
+})
--- src/data/calculate.ts
+++ src/data/calculate.ts
@@ -7,93 +7,93 @@ import {
   TheirMask,
   Voice,
   YourMask,
-} from "data/data";
+} from 'data/data'
 
 export interface CalculatorData {
   // Persistence
-  persistedAt?: number;
+  persistedAt?: number
 
   // Prevalence
-  topLocation: string;
-  subLocation: string;
-  population: string;
-  casesPastWeek: number;
-  casesIncreasingPercentage: number;
-  positiveCasePercentage: number | null;
+  topLocation: string
+  subLocation: string
+  population: string
+  casesPastWeek: number
+  casesIncreasingPercentage: number
+  positiveCasePercentage: number | null
 
   // Person risk
-  riskProfile: string;
-  interaction: string;
-  personCount: number;
+  riskProfile: string
+  interaction: string
+  personCount: number
 
   // Activity risk
-  setting: string;
-  distance: string;
-  duration: number;
-  theirMask: string;
-  yourMask: string;
-  voice: string;
+  setting: string
+  distance: string
+  duration: number
+  theirMask: string
+  yourMask: string
+  voice: string
 }
 
 export const defaultValues: CalculatorData = {
-  topLocation: "",
-  subLocation: "",
-  population: "",
+  topLocation: '',
+  subLocation: '',
+  population: '',
   casesPastWeek: 0,
   casesIncreasingPercentage: 0,
   positiveCasePercentage: 0,
 
-  riskProfile: "",
-  interaction: "",
+  riskProfile: '',
+  interaction: '',
   personCount: 0,
 
-  setting: "",
-  distance: "",
+  setting: '',
+  distance: '',
   duration: 0,
-  theirMask: "",
-  yourMask: "",
-  voice: "",
-};
+  theirMask: '',
+  yourMask: '',
+  voice: '',
+}
 
 // Replace any values that no longer exist with empty string (nothing selected).
 // This is used when restoring a previous saved scenario, in case we changed
 // the model in the meantime.
 export const migrateDataToCurrent = (
-  incomingData: Record<string, unknown>
+  incomingData: Record<string, unknown>,
 ): CalculatorData => {
-  const data: Partial<CalculatorData> = { ...incomingData };
+  const data: Partial<CalculatorData> = { ...incomingData }
   const fixOne = (
     table: { [key: string]: FormValue },
-    prop: keyof CalculatorData
+    prop: keyof CalculatorData,
   ) => {
-    const current = data[prop];
-    if (typeof current !== "string" || !(current in table)) {
-      delete data[prop];
+    const current = data[prop]
+    if (typeof current !== 'string' || !(current in table)) {
+      delete data[prop]
     }
-  };
-  fixOne(RiskProfile, "riskProfile");
-  fixOne(Interaction, "interaction");
-  fixOne(Setting, "setting");
-  fixOne(Distance, "distance");
-  fixOne(TheirMask, "theirMask");
-  fixOne(YourMask, "yourMask");
-  fixOne(Voice, "voice");
-  return { ...defaultValues, ...data };
-};
+  }
+  fixOne(RiskProfile, 'riskProfile')
+  fixOne(Interaction, 'interaction')
+  fixOne(Setting, 'setting')
+  fixOne(Distance, 'distance')
+  fixOne(TheirMask, 'theirMask')
+  fixOne(YourMask, 'yourMask')
+  fixOne(Voice, 'voice')
+  return { ...defaultValues, ...data }
+}
 
-const ONE_MILLION = 1e6; // One 'full' COVID
+const ONE_MILLION = 1e6 // One 'full' COVID
 
-export const MAX_ACTIVITY_RISK = 0.48;
-export const MAX_POINTS = 100000;
+export const MAX_ACTIVITY_RISK = 0.48
+export const MAX_POINTS = 100000
 
 // --------
 // our sheet at https://www.getguesstimate.com/models/16798
 // suggests that given known unknowns we could be about 3x off
 // in either direction
-export const ERROR_FACTOR = 3;
+export const ERROR_FACTOR = 3
 
 export const parsePopulation = (input: string): number =>
-  Number(input.replace(/[^0-9.e]/g, ""));
+  Number(input.replace(/[^0-9.e]/g, ''))
 
 // Convention: all of these functions return null if they determine
 // that we have insufficient data filled in to do the calculation.
@@ -101,178 +101,178 @@ export const parsePopulation = (input: string): number =>
 // local prevalence data a little more cleanly.
 
 export const calculateLocationReportedPrevalence = (
-  data: CalculatorData
+  data: CalculatorData,
 ): number | null => {
   try {
-    const population = parsePopulation(data.population);
+    const population = parsePopulation(data.population)
     if (population === 0) {
-      return null;
+      return null
     }
 
-    const lastWeek = data.casesPastWeek;
-    if (lastWeek === 0 && data.topLocation === "") {
+    const lastWeek = data.casesPastWeek
+    if (lastWeek === 0 && data.topLocation === '') {
       // If the data say zero cases, go with it; but if the user
       // entered zero cases, call it incomplete.
-      return null;
+      return null
     }
 
     // Additive smoothing, only relevant for super low case numbers
-    const prevalence = (lastWeek + 1) / population;
-    return prevalence;
+    const prevalence = (lastWeek + 1) / population
+    return prevalence
   } catch (e) {
-    return null;
+    return null
   }
-};
+}
 
 export const calculateLocationPersonAverage = (
-  data: CalculatorData
+  data: CalculatorData,
 ): number | null => {
   // Prevalence
 
-  const prevalence = calculateLocationReportedPrevalence(data);
+  const prevalence = calculateLocationReportedPrevalence(data)
   if (prevalence === null) {
-    return null;
+    return null
   }
 
   try {
-    let underreportingFactor;
+    let underreportingFactor
 
     // Under-reporting factor
     if (data.positiveCasePercentage === null) {
       // No positive test rate data available => assume the worst
-      underreportingFactor = 10;
+      underreportingFactor = 10
     } else if (data.positiveCasePercentage < 5) {
-      underreportingFactor = 6;
+      underreportingFactor = 6
     } else if (data.positiveCasePercentage < 15) {
-      underreportingFactor = 8;
+      underreportingFactor = 8
     } else {
-      underreportingFactor = 10;
+      underreportingFactor = 10
     }
 
-    const delayFactor = 1 + Math.max(0, data.casesIncreasingPercentage / 100);
+    const delayFactor = 1 + Math.max(0, data.casesIncreasingPercentage / 100)
 
     // --------
     // Points for "random person from X location"
-    const personRisk = prevalence * underreportingFactor * delayFactor;
+    const personRisk = prevalence * underreportingFactor * delayFactor
 
-    return personRisk * ONE_MILLION;
+    return personRisk * ONE_MILLION
   } catch (e) {
-    return null;
+    return null
   }
-};
+}
 
 export const calculatePersonRiskEach = (
   data: CalculatorData,
-  averagePersonRisk: number
+  averagePersonRisk: number,
 ): number | null => {
   try {
-    let risk;
-    if (data.riskProfile === "hasCovid") {
+    let risk
+    if (data.riskProfile === 'hasCovid') {
       // Special case COVID: they have a 100% chance of having it
-      risk = ONE_MILLION;
-    } else if (data.riskProfile === "") {
+      risk = ONE_MILLION
+    } else if (data.riskProfile === '') {
       // If risk profile isn't selected, call it incomplete
-      return null;
+      return null
     } else {
-      risk = averagePersonRisk;
-      risk *= RiskProfile[data.riskProfile].multiplier;
+      risk = averagePersonRisk
+      risk *= RiskProfile[data.riskProfile].multiplier
     }
-    return risk;
+    return risk
   } catch (e) {
-    return null;
+    return null
   }
-};
+}
 
 export const calculatePersonRisk = (
   data: CalculatorData,
-  averagePersonRisk: number
+  averagePersonRisk: number,
 ): number | null => {
   try {
-    let risk = calculatePersonRiskEach(data, averagePersonRisk);
+    let risk = calculatePersonRiskEach(data, averagePersonRisk)
     if (risk === null || data.personCount === 0) {
-      return null;
+      return null
     }
-    risk *= data.personCount;
-    return risk;
+    risk *= data.personCount
+    return risk
   } catch (e) {
-    return null;
+    return null
   }
-};
+}
 
 export const calculateActivityRisk = (data: CalculatorData): number | null => {
   try {
-    if (data.interaction === "") {
-      return null;
+    if (data.interaction === '') {
+      return null
     }
 
-    const repeatedInteraction = data.interaction == "repeated";
+    const repeatedInteraction = data.interaction == 'repeated'
 
-    let multiplier = 1;
-    multiplier *= Interaction[data.interaction].multiplier;
+    let multiplier = 1
+    multiplier *= Interaction[data.interaction].multiplier
 
     if (!repeatedInteraction) {
       if (data.duration === 0) {
-        return null;
+        return null
       }
       // If something isn't selected, use the "baseline" value (indoor, unmasked,
       // undistanced, regular conversation)
       const mulFor = (
         table: { [key: string]: FormValue },
-        given: string
-      ): number => (given === "" ? 1 : table[given].multiplier);
+        given: string,
+      ): number => (given === '' ? 1 : table[given].multiplier)
 
-      let effectiveDuration = data.duration;
-      multiplier *= mulFor(Distance, data.distance);
-      if (data.distance == "intimate") {
+      let effectiveDuration = data.duration
+      multiplier *= mulFor(Distance, data.distance)
+      if (data.distance == 'intimate') {
         // Even a brief kiss probably has a non-trivial chance of transmission.
-        effectiveDuration = Math.max(effectiveDuration, 60);
+        effectiveDuration = Math.max(effectiveDuration, 60)
       } else {
         // Being outdoors only helps if you're not literally breathing each others' exhalation.
-        multiplier *= mulFor(Setting, data.setting);
+        multiplier *= mulFor(Setting, data.setting)
       }
-      multiplier *= mulFor(TheirMask, data.theirMask);
-      multiplier *= mulFor(YourMask, data.yourMask);
-      multiplier *= mulFor(Voice, data.voice);
+      multiplier *= mulFor(TheirMask, data.theirMask)
+      multiplier *= mulFor(YourMask, data.yourMask)
+      multiplier *= mulFor(Voice, data.voice)
 
-      multiplier *= effectiveDuration / 60.0;
+      multiplier *= effectiveDuration / 60.0
     }
     if (multiplier > MAX_ACTIVITY_RISK) {
-      multiplier = MAX_ACTIVITY_RISK;
+      multiplier = MAX_ACTIVITY_RISK
     }
-    return multiplier;
+    return multiplier
   } catch (e) {
-    return null;
+    return null
   }
-};
+}
 
 export const calculate = (data: CalculatorData): number | null => {
   try {
-    let points;
+    let points
 
-    const averagePersonRisk = calculateLocationPersonAverage(data);
+    const averagePersonRisk = calculateLocationPersonAverage(data)
     if (averagePersonRisk === null) {
-      return null;
+      return null
     }
 
     // Person risk
-    points = calculatePersonRisk(data, averagePersonRisk);
+    points = calculatePersonRisk(data, averagePersonRisk)
     if (points === null) {
-      return null;
+      return null
     }
 
     // Activity risk
-    const activityRisk = calculateActivityRisk(data);
+    const activityRisk = calculateActivityRisk(data)
     if (activityRisk === null) {
-      return null;
+      return null
     }
-    points *= activityRisk;
+    points *= activityRisk
 
     if (points > MAX_POINTS) {
-      points = MAX_POINTS;
+      points = MAX_POINTS
     }
 
-    return points;
+    return points
   } catch (e) {
-    return null;
+    return null
   }
-};
+}
