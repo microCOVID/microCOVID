5815a7fd517156323e76e412fd4c94a40b9cf356
Jeremy Blanchard <blanchard.jeremy@gmail.com>
2020-10-11 17:40:08 -0700

    Add link inside new user introduction. Fix collapse transition on introduction.

--- src/components/FirstTimeUserIntroduction.tsx
+++ src/components/FirstTimeUserIntroduction.tsx
@@ -15,17 +15,18 @@ export function FirstTimeUserIntroduction(): React.ReactElement {
         {open ? <BsChevronDown /> : <BsChevronRight />} First time user?
       </Button>
       <Collapse in={open}>
-        <div id="first-time-user-introduction" className="collapse">
+        <div id="first-time-user-introduction" className="">
           The calculator introduces a new concept, the microCOVID. One
           microCOVID is a one-in-a-million chance of getting COVID. An activity
           that’s 20,000 microCOVIDs is very unsafe, as you have a 2% risk of
-          catching COVID *every time you do it*. An activity that’s 20
+          catching COVID <em>every time you do it</em>. An activity that’s 20
           microCOVIDs is relatively safe, as you could do it every week for a
           year and still have only about a 0.1% chance of getting COVID. If you
           want to keep your chance of getting COVID down below 1% a year, you’ll
           want your weekly budget of activities to sum to less than 192
-          microCOVIDs each week. We’ve created a handy spreadsheet [link] that
-          lets you keep track of your activities over time.
+          microCOVIDs each week. We’ve created a handy{' '}
+          <a href="/spreadsheet">spreadsheet</a> that lets you keep track of
+          your activities over time.
         </div>
       </Collapse>
     </>
