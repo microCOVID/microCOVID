73b08f32e03fb58f14f59e6a9c198859d315bbcc
Jeremy Blanchard <blanchard.jeremy@gmail.com>
2020-10-22 21:25:09 -0700

    Update precaution text

--- src/components/calculator/PointsDisplay.tsx
+++ src/components/calculator/PointsDisplay.tsx
@@ -238,13 +238,17 @@ export function ExplanationCard(props: {
             <em>These may or may not apply to your activity.</em>
             <ol className="mt-2">
               <li>
-                MODify your activities (make them Masked, Outdoors, Distanced)
+                <abbr title="Masked, Ourdoors, Distanced">MOD</abbr>ify your
+                activities (make them Masked, Outdoors, Distanced)
               </li>
               <li>
                 Wear the best mask you can get! [link “learn more about masks”]
               </li>
               <li>Visit public places during less crowded hours</li>
-              <li>Use delivery services to replace shopping trips</li>
+              <li>
+                Reduce shopping trips by doing one trip for multiple people,
+                using delivery services, or shopping online
+              </li>
             </ol>
           </>
         ) : (
@@ -259,21 +263,33 @@ export function ExplanationCard(props: {
                 as theirs
               </li>
               <li>
-                Ask them to MODify their activities (Masked, Outdoors,
-                Distanced)
+                Ask them to <abbr title="Masked, Ourdoors, Distanced">MOD</abbr>
+                ify their activities (Masked, Outdoors, Distanced)
               </li>
               <li>
-                Essential workers can wear a top quality mask N95 (or KN95) to
-                work
+                Essential workers can wear a top quality mask (e.g., N95 or
+                KN95) to work
               </li>
               <li>
-                Encourage them to limit their socializing to as few people as
+                Ask them to limit undistanced socializing to as few people as
                 possible
               </li>
               <li>
-                If they are very risky and you must be inside with them, you can
-                wear masks inside or isolate within the house by staying in
-                different rooms.
+                Encourage close contacts to isolate and get tested at the first
+                sign of COVID symptoms
+              </li>
+              <li>
+                If they have high COVID risk and you must be inside with them:
+                <ul>
+                  <li>Wear masks inside</li>
+                  <li>
+                    Maximize ventilation using open windows, fans blowing in
+                    outside air, and HEPA filters
+                  </li>
+                  <li>
+                    Isolate within the house by staying in different rooms.
+                  </li>
+                </ul>
               </li>
             </ol>
           </>
