a7f6dd4ecdba9a3a965b7f78d3649cf9ef787c74
Jeremy Blanchard <blanchard.jeremy@gmail.com>
2020-10-22 22:41:40 -0700

    Small cleanup tweaks

--- src/components/FirstTimeUserIntroduction.tsx
+++ src/components/FirstTimeUserIntroduction.tsx
@@ -21,12 +21,15 @@ export function FirstTimeUserIntroduction(): React.ReactElement {
           that’s 20,000 microCOVIDs is very unsafe, as you have a 2% risk of
           getting COVID <em>every time you do it</em>. An activity that’s 20
           microCOVIDs is relatively safe, as you could do it every week for a
-          year and still have only about a 0.1% chance of getting COVID. If you
-          want to keep your chance of getting COVID down below 1% a year, you’ll
-          want your weekly budget of activities to sum to less than 200
-          microCOVIDs each week. We’ve created a handy{' '}
-          <a href="/spreadsheet">spreadsheet</a> that lets you keep track of
-          your activities over time.
+          year and still have only about a 0.1% chance of getting COVID. We
+          decided an annual risk budget of a 1% chance of catching COVID per
+          year was reasonable for most healthy people who are not in contact
+          with vulnerable groups. To learn how we arrived at that number,{' '}
+          <a href="/paper/2-riskiness">click here</a>. If you want to follow
+          this 1%-risk-per-year budget, you’ll want your weekly budget of
+          activities to sum to less than about 200 microCOVIDs each week. We’ve
+          created a handy <a href="/spreadsheet">spreadsheet</a> that lets you
+          keep track of your activities over time.
         </div>
       </Collapse>
     </>
