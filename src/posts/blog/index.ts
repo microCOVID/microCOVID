265db265df13064877a10211ec2ca43d7132807f
Matt Bell <llebttam@gmail.com>
2020-10-30 14:10:59 -0700

    Update index.ts

--- src/posts/blog/index.ts
+++ src/posts/blog/index.ts
@@ -1,8 +1,8 @@
 import { PostMap } from 'posts/post'
 
-import post1 from './1-sample'
+import post1 from './ux-redesign'
 
 // Note: the keys in this map are the public URL for the post. Changing them will break links.
 export const pages: PostMap = {
-  '1-intro': post1,
+  '1-uxredesign': post1,
 }
