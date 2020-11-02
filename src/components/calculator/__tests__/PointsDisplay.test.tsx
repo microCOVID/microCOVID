afc45d70627c06fd6460c830cd74ec9f566a514b
Jeremy Blanchard <blanchard.jeremy@gmail.com>
2020-11-01 22:28:28 -0800

    Remove un-used imports

--- src/components/calculator/__tests__/PointsDisplay.test.tsx
+++ src/components/calculator/__tests__/PointsDisplay.test.tsx
@@ -1,4 +1,4 @@
-import { cleanup, fireEvent, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 
