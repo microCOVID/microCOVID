98dcf57c884c1a2de5d44d79542563a5877bfc03
Dominic Dagradi <ddagradi@gmail.com>
2020-11-02 09:23:30 -0800

    add setupTest, test types, remove unused library

--- src/components/calculator/__tests__/PointsDisplay.test.tsx
+++ src/components/calculator/__tests__/PointsDisplay.test.tsx
@@ -1,6 +1,5 @@
 import { render } from '@testing-library/react'
 import React from 'react'
-import '@testing-library/jest-dom/extend-expect'
 
 import PointsDisplay from 'components/calculator/PointsDisplay'
 
