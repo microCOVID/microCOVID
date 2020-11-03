d3e4f8bab0c9e5efb47e1bd2ea2666116e1dd395
Dominic Dagradi <ddagradi@gmail.com>
2020-11-02 17:53:08 -0800

    Scroll to top of screen when resetting (#460)
    Fixes #456

--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -67,6 +67,7 @@ export const Calculator = (): React.ReactElement => {
   }
 
   const resetForm = () => {
+    window.scrollTo(0, 0)
     localStorage.setItem(FORM_STATE_KEY, JSON.stringify(defaultValues))
     setCalculatorData(defaultValues)
     setScenarioName('')
