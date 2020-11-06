87b5129c974ed40d65a469ba1a43d78efe4511a3
Jeremy Blanchard <blanchard.jeremy@gmail.com>
2020-11-06 15:55:37 -0800

    Add basic test for revealing activity details section (#468)
    * Add basic test for revealing activity details section
    
    * Remove accidental duplicate test line
    
    * Bit of code cleanup. Reuse californiaoption constant

--- src/pages/__tests__/Calculator.test.tsx
+++ src/pages/__tests__/Calculator.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import React from 'react'
 import { AllProviders } from 'test/utils'
 
@@ -9,5 +9,21 @@ describe('calculator page', () => {
     const { getByText } = render(<Calculator />, { wrapper: AllProviders })
 
     expect(getByText(/constructed a calculator that lets/i)).toBeInTheDocument()
+  })
+
+  it('activity details section reveals after location is selected', () => {
+    const { getByText, queryByText, getByRole } = render(<Calculator />, {
+      wrapper: AllProviders,
+    })
+
+    const interactionTypeLabelQuery = /Is this a single activity or an ongoing relationship/i
+    expect(queryByText(interactionTypeLabelQuery)).not.toBeInTheDocument()
+    const topLocationBox = getByRole('combobox', { name: /Select location/i })
+    const californiaOption = getByText(/California/i) as HTMLOptionElement
+    fireEvent.change(topLocationBox, {
+      target: { value: californiaOption.value },
+    })
+    expect(californiaOption.selected).toBeTruthy()
+    expect(getByText(interactionTypeLabelQuery)).toBeInTheDocument()
   })
 })
