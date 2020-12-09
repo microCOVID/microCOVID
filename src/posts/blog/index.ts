5c411cb9fcd667f5dd45a2a0426786f61e6f1153
Benjamin Shaya <bshaya@alum.mit.edu>
2020-12-08 20:38:36 -0800

    Making url for test post consistent

--- src/posts/blog/index.ts
+++ src/posts/blog/index.ts
@@ -1,12 +1,12 @@
 import uxredesign from 'posts/blog/1-ux-redesign/ux-redesign'
 import thanksgiving from 'posts/blog/2-thanksgiving/thanksgiving'
 import hepafilters from 'posts/blog/3-hepa-filters/hepa-filters'
-import negativeTests from 'posts/blog/4-negative-tests/negativeTests'
+import negativetests from 'posts/blog/4-negative-tests/negativeTests'
 import { BlogPostMap } from 'posts/post'
 
 // Note: the keys in this map are the public URL for the post. Changing them will break links.
 export const pages: BlogPostMap = {
-  negativeTests,
+  negativetests,
   hepafilters,
   thanksgiving,
   uxredesign,
