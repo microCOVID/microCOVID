c74405589b419cc0a3fece9d00e8986f8447ff81
Benjamin Shaya <bshaya@alum.mit.edu>
2021-01-07 17:39:25 -0800

    Add alert

--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useMemo, useState } from 'react'
 import { Alert, Col, Row } from 'react-bootstrap'
 import { Trans, useTranslation } from 'react-i18next'
 import { BsLink45Deg } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
 import { encodeQueryParams, useQueryParams } from 'use-query-params'
 import 'pages/styles/Calculator.scss'
 
@@ -147,6 +148,10 @@ export const Calculator = (): React.ReactElement => {
 
   return (
     <div id="calculator">
+      <Alert className="alert-info">
+        We've adjusted parameters for the new COVID-19 variant, B117. Read more{' '}
+        <Link to="/blog/b117">here.</Link>
+      </Alert>
       <Row>
         <Col md="12" lg="8" id="calculator-introduction">
           <p>
