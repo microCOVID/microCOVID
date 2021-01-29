d0e6e5aab0fe024d6e877c2b61530c455b9db69c
Matt Bell <llebttam@gmail.com>
2021-01-28 23:18:16 -0800

    Masks blog post
    Masks blog post

--- src/posts/blog/index.ts
+++ src/posts/blog/index.ts
@@ -3,10 +3,12 @@ import thanksgiving from 'posts/blog/2-thanksgiving/thanksgiving'
 import hepafilters from 'posts/blog/3-hepa-filters/hepa-filters'
 import negativetests from 'posts/blog/4-negative-tests/negativeTests'
 import b117 from 'posts/blog/5-b117/b117'
+import masks from 'posts/blog/6-masks/masks'
 import { BlogPostMap } from 'posts/post'
 
 // Note: the keys in this map are the public URL for the post. Changing them will break links.
 export const pages: BlogPostMap = {
+  masks,
   b117,
   negativetests,
   hepafilters,
