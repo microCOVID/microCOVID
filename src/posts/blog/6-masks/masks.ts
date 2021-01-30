e2a1ae5fafcca79d0b07ffdb12f6d712b2e6dade
Matt Bell <llebttam@gmail.com>
2021-01-30 10:04:28 -0800

    Visual fix on masks blog post so images display with proper aspect ratio on mobile  (#706)
    * Visual fix so images display with proper aspect ratio on mobile
    
    * testing dropping height
    
    * Fixed all remaining image aspect ratios, removed float-right

--- src/posts/blog/6-masks/masks.ts
+++ src/posts/blog/6-masks/masks.ts
@@ -25,6 +25,14 @@ const date = 'January 28, 2021'
 const summary =
   "With caseloads near record highs and new, more infectious COVID variants starting to spread, it's time to upgrade your masks to N95 or better, both to protect yourself and to slow the spread of the disease"
 const content = `
+
+<style>
+img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
+
 It’s year 2 of the pandemic, and masks have gone from a strange new experience to a core part of public life (and a fashion accessory!).  With caseloads at record highs and a more infectious [new variant](b117) on the horizon, we felt it was time to build a concise review for which masks give you the most protection and when you want to use them.  
 
 ## What’s the really short version?
@@ -83,7 +91,7 @@ Now we’re going to go through masks by type, explaining what each type is usef
 * **Downsides:** Minimal protection
 
 <figure>
-<img src=${poorfittingmasks} width="550", height="387"/>
+<img src=${poorfittingmasks} width="550"/>
 </figure>
 
 Cloth masks were a great stopgap solution in spring and summer 2020 when the world was struggling to produce enough PPE for healthcare workers.  However, that’s since changed, and you’re usually better off wearing a mask that brings more protection.  
@@ -188,7 +196,7 @@ However, in order to actually get that full benefit, you need to do some things 
 **Exhalation valves:** It should be noted that some N95s come with an exhalation valve.  While this makes the mask easier to breathe through, it lets you expel air from your mouth without any filtration, which isn’t safe for the people around you.  However, you don’t need to throw these masks out; you can just tape over the exhalation valve (on the outside, so other people know) and then wear it like a normal N95, or just put a surgical mask over it.
 
 <figure>
-<img src=${exhalationvalvetape} width="531", height="258"/>
+<img src=${exhalationvalvetape} width="531"/>
 </figure>
 
 **How N95s work:** N95 (and KN95) masks work on a very different principle from cloth masks -- they have an electrostatic charge that attracts small airborne particles that pass through the mask.  Water destroys the electric charge, so getting the mask wet will make it stop working.  Separately, the mask will stop working effectively once too many particles have become caught in it.  Here’s a [cute video explanation](https://www.youtube.com/watch?v=eAdanPfQdCA).
@@ -212,7 +220,7 @@ However, in order to actually get that full benefit, you need to do some things 
 * **Downsides:** Large, expensive, can’t be washed, makes your voice sound funny, people will stare at you
 
 <figure>
-<img src=${p100} width="514", height="339"/>
+<img src=${p100} width="514"/>
 </figure>
 
 P100 respirators are designed for serious professional hazardous materials work — mold remediation, asbestos removal, toxic waste cleanup, tear gas attacks.  They’re the best protection you can get.  They have rubber seals that tightly cover your face, adjustable headstraps, and, best of all, filters designed to remove 99.97% of small particles from the air.  They’re designed to be comfortable and easy to breathe through for hours on end.  They won’t fog your glasses, and the inside of the mask won’t get moist even if you’re walking around or engaging in moderate exercise.  If you want to cut your risk of catching COVID way down while living in comfort, this is your tool.  
@@ -228,11 +236,11 @@ Since P100s were designed for hazardous environments as opposed to pandemics, th
 This modification is easy and takes no more than 5 minutes.  You’ll need your P100, a surgical mask, and some tape.  (If you don’t have a surgical mask, you can use a piece of cloth for this purpose, but surgical masks practically grow on trees at this point.)  Find the exhalation valve on the P100.  It should be obvious, but if not, put the mask on and feel where the air blows out of the mask when you breathe out.  Cut a piece of surgical mask that’s somewhat larger than the exhalation valve and then use the tape to attach it to the exhalation valve.  You’ll want to block as little of the exhalation valve with tape as possible since you want the surgical mask to filter this air instead of having the tape block it.  If you simply tape over the exhalation valve as you do with an N95, the mask will become very difficult to breathe through, as the air you exhale will have nowhere to go.  The inhalation filters often have one-way valves, and even if they don’t, you don’t want your moist breath degrading them.
 
 <figure>
-<img src=${p100tapedmoldex} width="527", height="265"/>
+<img src=${p100tapedmoldex} width="527"/>
 </figure>
 
 <figure>
-<img src=${p100taped3m} width="527", height="259"/>
+<img src=${p100taped3m} width="527"/>
 </figure>
 
 Aside from high-particulate scenarios such as wildfire smoke, P100 respirators can generally be used for an extended period of time spanning many cumulative full days of use. The filters only need to be replaced when they become wet, excessively dirty, or difficult to breathe through.  Some P100 filters have a stated lifetime of about 6 months once opened whether you use them or not; however it is unclear whether this applies to all P100 filters or just ones that have a VOC filtering capability.  In any case, a P100 that has been open for more than 6 months is still likely your best option if you can’t get a new one.  
@@ -252,7 +260,7 @@ Options include:
 ## Eye protection - Goggles and face shields 
 
 
-<figure class="float-right">
+<figure>
 <img src=${faceshield} width="243", height="243"/>
 </figure>
 
