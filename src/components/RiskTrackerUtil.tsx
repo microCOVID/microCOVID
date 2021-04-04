5d12b73c6227214686346031827dde459d8968c9
Jeremy Blanchard <blanchard.jeremy@gmail.com>
2021-04-04 13:26:28 -0700

    Move the version number next to the spreadsheet URL so we can change everything at once. (#821)

--- src/components/RiskTrackerUtil.tsx
+++ src/components/RiskTrackerUtil.tsx
@@ -4,6 +4,9 @@ import React from 'react'
 export const spreadsheetUrl =
   'https://docs.google.com/spreadsheets/d/1W3_RxaLrqILJae6Uw-I1D9opP5tKVA694OhdGusNwMs'
 // Do not include '/edit' at the end of the URL, because we append different paths like /copy or /template/preview depending on the use case
+export const riskTrackerVersion = '2.2.3'
+export const riskTrackerReleaseDate = '2021-03-16'
+// Reminder: Make sure to bump the version number that gets imported into their copies of the spreadsheet. Located at: /public/tracker/latest_version_number.csv
 
 export const mailchimpSubscribeUrl = 'http://eepurl.com/hb6y4T'
 
