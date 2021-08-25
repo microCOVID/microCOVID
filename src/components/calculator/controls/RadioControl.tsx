a1b6ab5322128df0b93339f3fc16696f4e557524
Kathy Garcia <yandereko@gmail.com>
2021-08-25 16:43:11 -0400

    Only show detail text for selected risk budget option (#1053)

--- src/components/calculator/controls/RadioControl.tsx
+++ src/components/calculator/controls/RadioControl.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Form } from 'react-bootstrap'
+import { Accordion, Form } from 'react-bootstrap'
 
 import ControlLabel from './ControlLabel'
 import { recordCalculatorOptionSelected } from 'components/Analytics'
@@ -23,28 +23,30 @@ export const RadioControl: React.FunctionComponent<{
       header={props.header}
       popover={props.popover}
     />
-    {Object.entries(props.source).map(([key, formValue], index) => (
-      <Form.Check key={index} id={props.id + index}>
-        <Form.Check.Input
-          type="radio"
-          name={props.id}
-          value={key}
-          onChange={() => {
-            recordCalculatorOptionSelected(props.id, key)
-            props.setter(key)
-          }}
-          defaultChecked={key === props.value.toString()}
-        />
-        <Form.Check.Label>
-          <strong>{formValue.label}</strong>
-        </Form.Check.Label>
-        {formValue.sublabel && (
-          <>
-            <Form.Text as="div">{formValue.sublabel}</Form.Text>
-          </>
-        )}
-      </Form.Check>
-    ))}
+    <Accordion activeKey={props.value.toString()}>
+      {Object.entries(props.source).map(([key, formValue], index) => (
+        <Form.Check key={index} id={props.id + index}>
+          <Form.Check.Input
+            type="radio"
+            name={props.id}
+            value={key}
+            onChange={() => {
+              recordCalculatorOptionSelected(props.id, key)
+              props.setter(key)
+            }}
+            defaultChecked={key === props.value.toString()}
+          />
+          <Form.Check.Label>
+            <strong>{formValue.label}</strong>
+          </Form.Check.Label>
+          {formValue.sublabel && (
+            <Accordion.Collapse eventKey={key}>
+              <Form.Text as="div">{formValue.sublabel}</Form.Text>
+            </Accordion.Collapse>
+          )}
+        </Form.Check>
+      ))}
+    </Accordion>
     {props.helpText && (
       <Form.Text id={props.id + 'HelpText'} muted>
         {props.helpText}
