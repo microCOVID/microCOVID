7d0aeacdb1672ca2470fea3ec247caa5ae9a7332
Jeremy Blanchard <blanchard.jeremy@gmail.com>
2021-08-27 09:21:34 -0700

    Fix microCOVID automation bot email address

--- scripts/prevalence_helper.sh
+++ scripts/prevalence_helper.sh
@@ -99,7 +99,7 @@ if [[ `git status --porcelain` ]]; then
   TODAY=$(date +%Y-%m-%d)
 
   git config --global user.name "microCOVID[bot]"
-  git config --global user.email "123456789+microcovid[bot]@users.noreply.github.com"
+  git config --global user.email "89503524+microcovid[bot]@users.noreply.github.com"
 
   git add -A
   git commit -am "Automatic prevalence update $TODAY"
