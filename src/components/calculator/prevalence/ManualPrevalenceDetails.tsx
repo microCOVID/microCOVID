7e3f194f881038cc1f8f2393c1202bc794b9ec23
Kathy Garcia <yandereko@gmail.com>
2021-09-14 23:27:38 -0400

    Fix

--- src/components/calculator/prevalence/ManualPrevalenceDetails.tsx
+++ src/components/calculator/prevalence/ManualPrevalenceDetails.tsx
@@ -91,83 +91,91 @@ export const ManualPrevalenceDetails: React.FunctionComponent<{
       <Card.Header className="details-header">
         <BsDash /> {t('calculator.prevalence.details_header')}
       </Card.Header>
-      <PrevalenceResult data={props.data} />
-      <PrevalenceField
-        id="reported-cases"
-        label={t('calculator.prevalence.last_week_cases')}
-        value={props.data.casesPastWeek || 0}
-        setter={(value) =>
-          props.setter({ ...props.data, casesPastWeek: parseInt(value || '') })
-        }
-        inputType="number"
-        className="hide-number-buttons"
-      />
-      <PrevalenceField
-        id="population"
-        label={t('calculator.prevalence.population')}
-        value={props.data.population}
-        setter={(value) => props.setter({ ...props.data, population: value })}
-        inputType="text"
-        className="hide-number-buttons"
-      />
-      <PrevalenceField
-        id="percent-increase"
-        label={t('calculator.prevalence.percent_increase_in_cases')}
-        value={props.data.casesIncreasingPercentage}
-        unit="%"
-        setter={(value) => {
-          props.setter({
-            ...props.data,
-            casesIncreasingPercentage: Number(value),
-          })
-        }}
-        inputType="number"
-        min={0}
-        className="hide-number-buttons"
-      />
-      <PrevalenceField
-        id="positive-test-rate"
-        label={t('calculator.prevalence.positive_case_percentage')}
-        value={
-          props.data.positiveCasePercentage
-            ? props.data.positiveCasePercentage.toString()
-            : ''
-        }
-        unit="%"
-        setter={(value) => {
-          props.setter({ ...props.data, positiveCasePercentage: Number(value) })
-        }}
-        inputType="number"
-        max={100}
-        min={0}
-        className="hide-number-buttons"
-      />
-      <PrevalenceField
-        id="vaccinated-rate"
-        label={t('calculator.prevalence.completed_vaccinations')}
-        value={
-          props.data.percentFullyVaccinated
-            ? props.data.percentFullyVaccinated.toString()
-            : ''
-        }
-        unit="%"
-        setter={(value) => {
-          props.setter({
-            ...props.data,
-            percentFullyVaccinated: Number(value),
-          })
-        }}
-        inputType="number"
-        max={100}
-        min={0}
-        className="hide-number-buttons"
-        readOnly={props.data.unvaccinatedPrevalenceRatio !== null}
-        helpText={
-          props.data.unvaccinatedPrevalenceRatio !== null
-            ? undefined
-            : t('calculator.prevalence.completed_vaccinations_tooltip')
-        }
-      />
+      <Card.Body>
+        <PrevalenceResult data={props.data} />
+        <PrevalenceField
+          id="reported-cases"
+          label={t('calculator.prevalence.last_week_cases')}
+          value={props.data.casesPastWeek || 0}
+          setter={(value) =>
+            props.setter({
+              ...props.data,
+              casesPastWeek: parseInt(value || ''),
+            })
+          }
+          inputType="number"
+          className="hide-number-buttons"
+        />
+        <PrevalenceField
+          id="population"
+          label={t('calculator.prevalence.population')}
+          value={props.data.population}
+          setter={(value) => props.setter({ ...props.data, population: value })}
+          inputType="text"
+          className="hide-number-buttons"
+        />
+        <PrevalenceField
+          id="percent-increase"
+          label={t('calculator.prevalence.percent_increase_in_cases')}
+          value={props.data.casesIncreasingPercentage}
+          unit="%"
+          setter={(value) => {
+            props.setter({
+              ...props.data,
+              casesIncreasingPercentage: Number(value),
+            })
+          }}
+          inputType="number"
+          min={0}
+          className="hide-number-buttons"
+        />
+        <PrevalenceField
+          id="positive-test-rate"
+          label={t('calculator.prevalence.positive_case_percentage')}
+          value={
+            props.data.positiveCasePercentage
+              ? props.data.positiveCasePercentage.toString()
+              : ''
+          }
+          unit="%"
+          setter={(value) => {
+            props.setter({
+              ...props.data,
+              positiveCasePercentage: Number(value),
+            })
+          }}
+          inputType="number"
+          max={100}
+          min={0}
+          className="hide-number-buttons"
+        />
+        <PrevalenceField
+          id="vaccinated-rate"
+          label={t('calculator.prevalence.completed_vaccinations')}
+          value={
+            props.data.percentFullyVaccinated
+              ? props.data.percentFullyVaccinated.toString()
+              : ''
+          }
+          unit="%"
+          setter={(value) => {
+            props.setter({
+              ...props.data,
+              percentFullyVaccinated: Number(value),
+            })
+          }}
+          inputType="number"
+          max={100}
+          min={0}
+          className="hide-number-buttons"
+          readOnly={props.data.unvaccinatedPrevalenceRatio !== null}
+          helpText={
+            props.data.unvaccinatedPrevalenceRatio !== null
+              ? undefined
+              : t('calculator.prevalence.completed_vaccinations_tooltip')
+          }
+        />
+      </Card.Body>
     </Card>
   )
 }
