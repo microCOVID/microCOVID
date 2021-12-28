977af96b807e9ce14ff1cc9f1b53f20a5e86fd53
Kathy Garcia <yandereko@gmail.com>
2021-12-28 05:28:44 -0500

    Set up Sentry for both backend and frontend

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,20 @@
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './i18n'
 
 import { App } from './App'
+
+Sentry.init({
+  dsn:
+    'https://20a4fef5bf06400eac36928f803e6097@o1100628.ingest.sentry.io/6125912',
+  integrations: [new Integrations.BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
