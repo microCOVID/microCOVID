83fd7941f5f3de35002b40187c24ab4a4f7384a7
Josiah Roberts <josiah.david.roberts@gmail.com>
2022-04-12 18:59:17 -0700

    Try to maximize readability

--- update_prevalence.py
+++ update_prevalence.py
@@ -701,7 +701,10 @@ class AppLocation(pydantic.BaseModel):
     def prevalenceRatio(self) -> float:
         DAY_0 = datetime(2020, 2, 12)
         day_i = (datetime.now() - DAY_0).days
-        positivityRate = min(self.positiveCasePercentage or 100, 100)
+        positivityRate = self.positiveCasePercentage or 100
+
+        if positivityRate > 100:
+            positivityRate = 100
 
         if positivityRate < 0:
             print_and_log_to_sentry(f"Warning: Positivity rate is negative: {positivityRate}")
