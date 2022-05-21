660237972462326a6f4e9bcac4c64e4fc6260a5e
Vince Broz <vince@broz.cc>
2022-05-20 21:14:51 -0400

    Remove legacy global PrevalenceDataDate variable (#1442)
    Variable has been unused since updatedAt was added.

--- src/data/locationHelpers.ts
+++ src/data/locationHelpers.ts
@@ -1,4 +1,4 @@
-import { Locations, PrevalenceDataDate } from 'data/location'
+import { Locations } from 'data/location'
 
 interface PrevalanceData {
   population: string
@@ -26,9 +26,8 @@ export function dataForLocation(location: string): PrevalanceData {
         locationData.positiveCasePercentage === null
           ? null
           : Math.round(locationData.positiveCasePercentage * 10) / 10,
-      // TODO just use locationData.updatedAt once update_prevalence has added it
       prevalanceDataDate: new Date(
-        locationData.updatedAt || PrevalenceDataDate,
+        locationData.updatedAt
       ),
       percentFullyVaccinated: locationData.completeVaccinations
         ? Math.round((locationData.completeVaccinations / population) * 100)
